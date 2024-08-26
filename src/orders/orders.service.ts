017db943bdec47e9c564829db404f5a0da308569
JaviSrz <ljsuarez7@gmail.com>
2024-08-26 18:36:19 +0200

    Implementación find one

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -22,8 +23,19 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     return `This action returns all orders`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+
+    const order = await this.order.findFirst({where: {id}});
+
+    if(!order){
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Order with id ${id} not found`
+      });
+    }
+
+    return order;
+
   }
 
   changeOrderStatus() {
